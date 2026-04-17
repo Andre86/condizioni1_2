4ca79e2b86a70bad528b4dd7d34393bbd73760c2
Andrea Lerose <a.lerose@itispa.com>
2026-04-17 16:12:48 +0200

    inizio lavori

--- main.js
+++ main.js
@@ -1,43 +1,46 @@
-let voto = 333;
+// let voto = 333;
 
-// if(voto <= 18){
-//     console.log('Insufficiente');
+// // if(voto <= 18){
+// //     console.log('Insufficiente');
     
-// }else if(voto > 18 && voto <=21){
-//     console.log('Sufficiente');
-// }else if(voto > 21 && voto <=24){
-//     console.log('Buono');
-// }else if(voto > 24 && voto <=27){
-//     console.log('Distinto');
-// }else if(voto > 27 && voto <=29){
-//     console.log('Ottimo');
-// }else if(voto == 30){
-//     console.log('Eccellente');
-// }else{
-//     console.log(`Voto non valido!`)
+// // }else if(voto > 18 && voto <=21){
+// //     console.log('Sufficiente');
+// // }else if(voto > 21 && voto <=24){
+// //     console.log('Buono');
+// // }else if(voto > 24 && voto <=27){
+// //     console.log('Distinto');
+// // }else if(voto > 27 && voto <=29){
+// //     console.log('Ottimo');
+// // }else if(voto == 30){
+// //     console.log('Eccellente');
+// // }else{
+// //     console.log(`Voto non valido!`)
+// // }
+
+// let giudizio; 
+// switch(true){
+//     case voto <= 18:
+//         giudizio = 'Insufficiente'
+// break;
+// case voto > 18 && voto <= 21:
+//         giudizio = 'Sufficiente'
+// break;
+// case voto > 21 && voto <= 24:
+//         giudizio = 'Buono'
+// break;
+// case voto > 24 && voto <= 27:
+//         giudizio = 'Distinto'
+// break;
+// case voto > 27 && voto <= 29:
+//         giudizio = 'Ottimo'
+// break;
+// case voto == 30:
+//         giudizio = 'Eccellente'
+// break;
+//     break;
 // }
 
-let giudizio; 
-switch(true){
-    case voto <= 18:
-        giudizio = 'Insufficiente'
-break;
-case voto > 18 && voto <= 21:
-        giudizio = 'Sufficiente'
-break;
-case voto > 21 && voto <= 24:
-        giudizio = 'Buono'
-break;
-case voto > 24 && voto <= 27:
-        giudizio = 'Distinto'
-break;
-case voto > 27 && voto <= 29:
-        giudizio = 'Ottimo'
-break;
-case voto == 30:
-        giudizio = 'Eccellente'
-break;
-    break;
-}
+// console.log(giudizio);
 
-console.log(giudizio);
+// SU - esercizio precedente ---------------------
+
